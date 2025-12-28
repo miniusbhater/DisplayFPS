ee3bdffac400a2a24b5e69d6c00c29350cd61203
Rose <miniusbhater@gmail.com>
2025-12-28 22:10:32 +0000

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿using Assets.Scripts.Flight.Demo;
+using Assets.Scripts.UI;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Rewired;
+using Steamworks;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+
+namespace DisplayFPS
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private float deltaTime = 0.0f;
+        private GUIStyle style;
+        void Awake()
+        {
+            
+        }
+
+        void Start()
+        {
+           
+        }
+      
+
+        void Update()
+        {
+         
+        }
+
+        void OnGUI()
+        {
+            if (style == null)
+            {
+                style = new GUIStyle(GUI.skin.label)
+                {
+                    fontSize = 20
+                };
+            }
+
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+            int fps = Mathf.RoundToInt(1.0f / deltaTime);
+
+            if (fps < 40) style.normal.textColor = Color.red;
+            else if (fps < 60) style.normal.textColor = Color.yellow;
+            else style.normal.textColor = Color.green;
+
+            GUI.Label(new Rect(2470, 10, 300, 40), $"{fps} FPS", style);
+        }
+
+    }
+}
+
+
+
+    
+
---
+++ PluginInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayFPS
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.miniusbhater.SimplePlanes2.DisplayFPS";
+        public const string Name = "DisplayFPS";
+        public const string Version = "1.1.0";
+    }
+}
+
